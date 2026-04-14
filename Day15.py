cbefe0307e383d7960f55674c8f15828b1fc3bf7
Adarsha B M <adarsha23961@gmail.com>
2026-04-14 12:04:55 +0530

    Update Day15.py

--- Day15.py
+++ Day15.py
@@ -4,10 +4,10 @@ def greet():
     print("Hello good mornig") 
  
 greet()
-
+ 
 
 def marriage(boy, girl):
-    print(f"boy is {boy}")
+    print(f"boy is {boy}") 
     print(f"girl is {girl}")  
     print(f"{boy} married {girl}")
 
